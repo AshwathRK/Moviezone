441e481b4a5fbc65738ba84d265858d6d8103051
Ashwathaman <ashwathaman.r6@gmail.com>
2025-04-10 14:19:42 +0530

    path change

--- src/Components/PopulerIntrests.jsx
+++ src/Components/PopulerIntrests.jsx
@@ -8,70 +8,70 @@ export default function PopulerIntrests() {
             </div>
             <div className='popcards grid gap-3 justify-center'>
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/superHeros.jpeg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/superHeros.jpeg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>SuperHeros</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Fantasy.jpg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Fantasy.jpg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Fantasy</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/DarkComedy.jpg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/DarkComedy.jpg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Dark Comedy</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Thriller.webp" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Thriller.webp" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Thriller</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/commedy.webp" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/commedy.webp" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Comedy</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Anime.jpeg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Anime.jpeg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Anime</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Biography.jpg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Biography.jpg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Biography</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Action.webp" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Action.webp" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Action</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Adventure.jpg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Adventure.jpg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Adventure</h5>
                     </div>
                 </div>
 
                 <div className='populercards rounded-xl'>
-                    <img src="./public/Category/Romance.jpg" className='w-full h-65 object-cover rounded-t-xl' />
+                    <img src="./Category/Romance.jpg" className='w-full h-65 object-cover rounded-t-xl' />
                     <div className='w-full h-18 flex justify-center items-center'>
                         <h5 className='poppins-bold text-white'>Romance</h5>
                     </div>
