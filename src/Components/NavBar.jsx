77654cd7daf14876784a47e8eb432a95fe6e9ba4
Ashwathaman <ashwathaman.r6@gmail.com>
2025-04-10 15:28:50 +0530

    small screen button commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -36,7 +36,7 @@ export default function NavBar() {
     };
     return (
         <nav className='grid relative navbarColorGradiant lg:grid-cols-12 lg:grid-rows-1 lg:px-5 md:px-3 h-16' data-bs-theme="dark">
-                <a onClick={()=>{navigate("/");}} className='col-span-2 flex items-center justify-center'><img className='box-border w-20 h-12' src="logo_full_white.png" alt="main logo" /></a>
+            <a onClick={() => { navigate("/"); }} className='col-span-2 flex items-center justify-center'><img className='box-border w-20 h-12' src="logo_full_white.png" alt="main logo" /></a>
             <div className='col-span-6 col-start-3 flex items-center'>
                 <div className='searchBar w-full bg-white h-2/3 rounded-sm grid grid-cols-12 grid-rows-1 '>
                     <input value={searchEvent} onChange={(e) => { setSearchEvent(e.target.value) }} className='dropdown col-span-10 shrink col-start-1 outline-none poppins-semibold px-3' placeholder='Looking for a great movie? Discover it on MovieZone!' />
@@ -59,16 +59,26 @@ export default function NavBar() {
                     </button>
                 </div>
             </div>
-            <div className=' containerbtn col-span-4 col-start-3 col-start-9 flex items-center justify-center'>   
-                <button onClick={()=>{navigate("/");}} className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./home.png" className='mx-2 watchlist w-4' /><span className='watchList'>Home</span></button>
-                <button onClick={()=>{navigate("/movielist");}}  className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./list.png" className='mx-2 watchlist w-4' /><span className='watchList'>Movie List</span></button>
+            <div className=' containerbtn col-span-4 col-start-3 col-start-9 flex items-center justify-center'>
+                <button onClick={() => { navigate("/"); }} className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./home.png" className='mx-2 watchlist w-4' /><span className='watchList'>Home</span></button>
+                <button onClick={() => { navigate("/movielist"); }} className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./list.png" className='mx-2 watchlist w-4' /><span className='watchList'>Movie List</span></button>
                 <button onClick={SearchDisplay} className='searchbtn sm:hidden poppins-semibold flex items-center justify-around px-2'><img src="search_icon.png" className='w-auto h-4 searchicon' /></button>
-                <button onClick={()=>{navigate("/watchlist");}} className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./watchlist-dark.png" className='mx-2 watchlist w-5' /><span className='watchList'>Watchlist</span></button>
+                <button onClick={() => { navigate("/watchlist"); }} className='text-white poppins-semibold btn-watchlist h-10 px-3 flex items-center'> <img src="./watchlist-dark.png" className='mx-2 watchlist w-5' /><span className='watchList'>Watchlist</span></button>
             </div>
 
             <div className={`${searchBy} sm:hidden absolute w-full bg-white h-2/3 grid grid-cols-12 grid-rows-1`}>
                 <input className='dropdown col-span-8 col-start-1 outline-none poppins-requler px-3' placeholder='Looking for a great movie? Discover it on MovieZone!' />
-                <button className='btnSearch flex flex-none justify-around col-span-2 col-start-9 text-white items-center poppins-semibold'><img src="search_icon.png" className='h-4 searchicon' />Search</button>
+                <button onClick={() => {
+                    getValuefromInput();
+                }} className='btnSearch flex flex-none justify-around col-span-2 col-start-9 text-white items-center poppins-semibold'>{loading ? (
+                    <div className="none spinner-border text-light" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                ) : (
+                    <span className="flex justify-center items-center">
+                        <img src="search_icon.png" className="h-4 mx-2 searchicon" />
+                    </span>
+                )}</button>
                 <button onClick={SearchDisplay} className='btncancel flex flex-none bg-gray-600 justify-around col-span-2 col-start-11 text-white items-center poppins-semibold'>X cancel</button>
             </div>
         </nav>
